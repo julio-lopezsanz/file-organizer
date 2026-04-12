6c0bec98b3e8002848bbb265e44bba226bd9a205
Julio C Lopez <90801533+julio-lopezsanz@users.noreply.github.com>
2026-04-12 17:43:19 -0600

    feat: implement basic file organization logic

---
+++ main.py
@@ -0,0 +1,46 @@
+"""
+Automatic file organizer script.
+
+This script automates the process of monitoring a directory (such as Downloads)
+and organizing incoming files into specific subfolders based on their 
+file extensions using pathlib and watchdog.
+"""
+import shutil
+from pathlib import Path
+
+#Supported file extensions and the destination folder for each one
+rules = {
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".png": "Images",
+    ".gif": "Images",
+    ".doc": "Documents",
+    ".docx": "Documents",
+    ".pdf": "Documents",
+    ".txt": "Documents",
+    ".mp3": "Audio",
+    ".wav": "Audio",
+    ".aac": "Audio",
+    ".mp4": "Video",
+    ".avi": "Video",
+    ".mkv": "Video",
+    ".zip": "Archives",
+    ".rar": "Archives",
+    ".7z": "Archives"
+}
+root_dir = Path("/Users/lopez/Downloads")
+
+#Iterate only through files, ignoring folders and the script itself
+for file in root_dir.iterdir():
+    if file == Path(__file__):
+        continue
+    if not file.is_file():
+        continue
+
+    extension = file.suffix.lower() # Standardize to avoid issues with .JPG or .PDF files
+
+    if extension in rules:
+        des_dir = root_dir / rules[extension]
+        des_dir.mkdir(exist_ok=True)# It does not return an error if the folder already exists
+        shutil.move(file, des_dir)
+        print(f"Move: {file.name} -> {rules[extension]}")
